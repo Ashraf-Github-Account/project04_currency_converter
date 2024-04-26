e914f308c6b2c981250c4540a5a3fe9c7e709142
Muhammad Ashraf <164771322+Ashraf-Github-Account@users.noreply.github.com>
2024-04-26 07:11:32 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,51 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.blue.bold("\n \t Welcome to 'Dream-Build'- Currency Convertor\n"));
+
+
+// Define the rate of currencies and thier exchange rate
+
+let exchange_rate:any = {
+    "USD": 1,       // Base currency
+    "EUR": 0.9,     // European (Euro)
+    "JYP": 113,     // japenese Currency (Yen)
+    "CAD": 1.3,     // Canadian Dollar
+    "AUD": 1.65,    // Australian Dollar
+    "PKR": 277.70,     // Pakistan Rupees
+// Add more currencies and their exchange rates here
+}
+// From the user to select currencies to convert from and to
+let user_answar = await inquirer.prompt([
+    {
+        name: "from_Currency",
+        type: "list",
+        message: "Select the currency to convert from",
+        choices: ["USD", "EUR", "JYP", "CAD", "AUD", "PKR"]
+
+    },
+    {
+        name: "to_currency",
+        type: "list",
+        message: "Select the currency to convert into:",
+        choices: ["USD", "EUR", "JYP", "CAD", "AUD", "PKR"]
+    },
+    {
+        name: "amount",
+        type: "input",
+        message: "Enter the amount to convert:"
+    }
+]);
+
+// Perform currency conversion by using formula
+let from_amount = exchange_rate[user_answar.from_Currency];
+let to_amount = exchange_rate[user_answar.to_currency];
+let amount = user_answar.amount 
+
+// formula of conversion
+let base_amount = amount / from_amount
+let converted_amount = base_amount * to_amount
+
+// Display the converted amount
+console.log(`Converted Amount = ${chalk.green(converted_amount.toFixed(2))}`);
+
